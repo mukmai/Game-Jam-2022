c7bd74662338c85e398ab63f68429cf995970ad2
stella-hydrangea <93139623+stella-hydrangea@users.noreply.github.com>
2022-01-29 11:59:42 +0800

    update on LightReciever

--- Assets/Scripts/GameplayComponent/WaveReceiver.cs
+++ Assets/Scripts/GameplayComponent/WaveReceiver.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class WaveReceiver : LightReceiver
 {
-    
+    public virtual void OnCollisionEnter2D(Collision2D collision)
+    {
+        WaveLightRay wave = collision.collider.GetComponent<WaveLightRay>();
+        //Vector3[] collisionPoints = wave.GetPositions();
+        if(wave != null) 
+        {
+            _cloudParticles.Play();
+            _hitByWave =true;
+        }
+        else
+        {
+            _hitByWave = false;
+        }
+        return;
+    }
 }
