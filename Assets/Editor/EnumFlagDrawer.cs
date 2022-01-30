c1dd77e61a6276e582206760670cf1350a2b9462
Deacon <deaconkum@ymail.com>
2022-01-30 15:33:08 +0800

    fixed build error and added build version

---
+++ Assets/Editor/EnumFlagDrawer.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(EnumFlagAttribute))]
+public class EnumFlagDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EnumFlagAttribute flagSettings = (EnumFlagAttribute)attribute;
+        Enum targetEnum = (Enum)fieldInfo.GetValue(property.serializedObject.targetObject);
+
+        string propName = flagSettings.name;
+        if (string.IsNullOrEmpty(propName))
+            propName = ObjectNames.NicifyVariableName(property.name);
+
+        EditorGUI.BeginProperty(position, label, property);
+        Enum enumNew = EditorGUI.EnumMaskPopup(position, propName, targetEnum);
+        property.intValue = (int)Convert.ChangeType(enumNew, targetEnum.GetType());
+        EditorGUI.EndProperty();
+    }
+}
